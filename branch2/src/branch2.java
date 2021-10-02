953defc1cccd26c434b226fcb11b95a7a0d1ab3c
zhaoxiao <1292703250@qq.com>
2021-10-03 01:19:58 +0800

    9

--- branch2/src/branch2.java
+++ branch2/src/branch2.java
@@ -1,7 +1,6 @@
 public class branch2 {
     public static void main(String[] args) {
 
-        System.out.println("我是newbranch分支的代码");
-        System.out.println("我是newbranch分支的代码");
+        System.out.println("我是主分支master的代码");
     }
 }
